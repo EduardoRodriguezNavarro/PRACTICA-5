d68c81b9473b6830143e85db8e6bd33f716c9556
EduardoRodriguezNavarro <113403639+EduardoRodriguezNavarro@users.noreply.github.com>
2023-01-25 14:33:09 -0600

    CORRECCIÓN
    No habia podido probarla con el osciloscopio, por lo que tenía un error que no habia visualizado hasta el momemento de correrla conectada al osciloscopio. El error consistió en la mascara colocada para la configuración del PWM.

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include "lib/include.h"
 
+//____________________________________________________________________Experimento 1
+/*Usando el modulo 0 de PWM con una frecuencia de reloj del sistema de 50,000,000 Hz
+ * junto con el generador 1  habilitar alguno de los pwm's asociados y obtener un PWM
+ * cuya frecuencia sea de 10KHz
+ */
+
 int main(void)
 {
 
-    uint16_t Result[2];
-    uint16_t duty[2];
-    Configurar_PLL();  //Confiuracion de velocidad de reloj
-    Configura_Reg_ADC0();
-    Configurar_UART0();
-    Configura_Reg_PWM1(1000);//Configuro a 1khz el pwm
+    //uint16_t Result[2];
+    //uint16_t duty[2];
+    Configurar_PLL(_50MHZ);  //AQUÍ SE CONFIGURA LA VELOCIDAD DEL RELOJ
+    //Configura_Reg_ADC0();
+    //Configurar_UART0();
+    Configura_Reg_PWM0(8);//SE MANDA A LLAMAR LA FUNCIÓN QUE CONFIGURA AL PWM A 10 kHz 
     while(1)
     {
-        ADC0_InSeq2(Result,duty); //llamada a la conversion por procesador
+        /*ADC0_InSeq2(Result,duty); 
         PWM0->_1_CMPB = duty[0];
         PWM0->_1_CMPA = duty[1];
-        PWM0->_0_CMPA = 15000;
+        PWM0->_0_CMPA = 15000;*/
         
     }
 
